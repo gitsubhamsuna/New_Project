4a65e17426b775010a62409ce86b4dcfcf3b7382
gitsubhamsuna <134761761+gitsubhamsuna@users.noreply.github.com>
2023-05-27 18:05:23 +0530

    Update ReverseNumber.java
    i have change some code

--- src/main/java/Java_Project/ReverseNumber.java
+++ src/main/java/Java_Project/ReverseNumber.java
@@ -9,11 +9,9 @@ public class ReverseNumber {
 		int m=n;
 		int temp=0;
 		while(n!=0) {
-			int r=n%10;
-			temp=temp*10+r;
+			temp=temp*10+n%10;
 			n=n/10;
 		}
 		System.out.println(m+" is reverse : "+temp);
 	}
-
 }
